4310cba8664d36446fdc9fedabad10a20752321d
remmikim <139316018+remmikim@users.noreply.github.com>
2023-07-26 10:02:14 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -53,6 +53,14 @@ const CONFIG = {
       "issue-term": "og:title",
       label: "💬 Utterances",
     },
+  },
+    cusdis: {
+    enable: false,
+    config: {
+      host: "https://cusdis.com",
+      appid: "", // Embed Code -> data-app-id value
+    },
+  },
   isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
   revalidateTime: 21600 * 7, // revalidate time for [slug], index
 }
