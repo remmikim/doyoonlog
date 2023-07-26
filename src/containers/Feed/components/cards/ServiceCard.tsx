a2539e2250eea3e039edb2c80e71f7c30f855c5e
remmikim <139316018+remmikim@users.noreply.github.com>
2023-07-26 09:34:20 +0900

    Update ServiceCard.tsx

--- src/containers/Feed/components/cards/ServiceCard.tsx
+++ src/containers/Feed/components/cards/ServiceCard.tsx
@@ -14,7 +14,7 @@ const ServiceCard: React.FC = () => {
           target="_blank"
           className="p-3 hover:bg-gray-100 dark:hover:bg-zinc-700 rounded-2xl cursor-pointer flex items-center gap-3 text-gray-500 dark:text-white hover:text-black dark:hover:text-white"
         >
-          <AiFillCodeSandboxCircle className="text-2xl" />
+          <AiFillGithub className="text-2xl" />
           <div className="text-sm">{CONFIG.projects[0].name}</div>
         </a>
       </ul>
