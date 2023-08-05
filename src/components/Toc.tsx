3d17051f9241792e8348b81497ee18b9c5b05fe5
remmikim <139316018+remmikim@users.noreply.github.com>
2023-08-05 12:25:56 +0900

    Update Toc.tsx

--- src/components/Toc.tsx
+++ src/components/Toc.tsx
@@ -7,3 +7,13 @@ const Toc = () => {
 }
 
 export default Toc.
+useEffect(() => {
+    const observer = getIntersectionObserver(setCurrentId);
+    const headingElements = Array.from(document.querySelectorAll('h2, h3'));
+
+    setHeadingEls(headingElements);
+
+    headingElements.map((header) => {
+      observer.observe(header);
+    });
+  }, []);
