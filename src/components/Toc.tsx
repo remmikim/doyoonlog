289f7fd279e6d06aed1c7934edb10ddc2089b6b0
remmikim <139316018+remmikim@users.noreply.github.com>
2023-08-05 12:16:06 +0900

    Create Toc.tsx

---
+++ src/components/Toc.tsx
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+
+const Toc = () => {
+  const [currentId, setCurrentId] = useState<string>('');
+  const [headingEls, setHeadingEls] = useState<Element[]>([]);
+  return ...
+}
+
+export default Toc.
