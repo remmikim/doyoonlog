05140001c17d55dd92e19fb1d591503c9f179c6b
remmikim <139316018+remmikim@users.noreply.github.com>
2023-08-05 13:18:13 +0900

    Update Toc.tsx

--- src/components/Toc.tsx
+++ src/components/Toc.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { getIntersectionObserver } from 'src/libs/observer.ts';
+import { getIntersectionObserver } from 'src/libs/observer';
 
 const Toc = () => {
   const [currentId, setCurrentId] = useState<string>('');
