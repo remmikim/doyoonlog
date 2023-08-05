5d26e2477aa7821bdb3f0f1702d26bd6fc78b575
remmikim <139316018+remmikim@users.noreply.github.com>
2023-08-05 12:43:18 +0900

    Update Toc.tsx

--- src/components/Toc.tsx
+++ src/components/Toc.tsx
@@ -1,9 +1,10 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const Toc = () => {
   const [currentId, setCurrentId] = useState<string>('');
   const [headingEls, setHeadingEls] = useState<Element[]>([]);
-  return useEffect(() => {
+
+  useEffect(() => {
     const observer = getIntersectionObserver(setCurrentId);
     const headingElements = Array.from(document.querySelectorAll('h2, h3'));
 
@@ -13,6 +14,14 @@ const Toc = () => {
       observer.observe(header);
     });
   }, []);
+
+  // TODO: Render the TOC based on headingEls and currentId
+  
+  return (
+    <div>
+      {/* Render your Table of Contents here */}
+    </div>
+  );
 }
 
-export default Toc.
+export default Toc;
