5edc0569bf24df45583beb03ad18af15ccea4a1d
remmikim <139316018+remmikim@users.noreply.github.com>
2023-08-05 12:15:23 +0900

    Create observer.ts

---
+++ src/libs/observer.ts
@@ -0,0 +1,7 @@
+export const getIntersectionObserver = () => {
+  const observer = new IntersectionObserver( // observer 생성
+    (entries) => { console.log(entries) }, // callback
+    options // options
+  );
+  return observer;
+};
