87ba27119df77bec385a1ffdddf317858e2e2867
remmikim <139316018+remmikim@users.noreply.github.com>
2023-08-06 09:46:58 +0900

    Create Toc.tsx

---
+++ src/components/Toc.tsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import { getIntersectionObserver } from '../lib/observer';
+
+const Toc = () => {
+  const [currentId, setCurrentId] = useState<string>('');
+  const [headingEls, setHeadingEls] = useState<Element[]>([]);
+
+  useEffect(() => {
+    const observer = getIntersectionObserver(setCurrentId);
+    const headingElements = Array.from(document.querySelectorAll('h2, h3'));
+
+    setHeadingEls(headingElements);
+
+    headingElements.forEach((header) => {
+      observer.observe(header);
+    });
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
+  // 여기서 실제 목차를 렌더링하는 JSX를 작성하세요.
+
+  return (
+    <div>
+      {/* 목차 렌더링 JSX */}
+    </div>
+  );
+};
+
+export default Toc;
