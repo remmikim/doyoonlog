4668ed614960d15f7389501262916dceeccadcfe
remmikim <139316018+remmikim@users.noreply.github.com>
2023-08-18 14:48:00 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -63,7 +63,7 @@ const CONFIG = {
     },
   },
   cusdis: {
-    enable: true,
+    enable: false,
     config: {
       host: "https://cusdis.com",
       appid: "d8394c64-d0f6-4e30-97ee-30c763eb0e66", // Embed Code -> data-app-id value
